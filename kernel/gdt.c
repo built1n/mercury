b3b7066f899a6259779f97a2304152aa7fe18331
Drew DeVault <sir@cmpwn.com>
2014-10-11 10:56:13 -0600

    Load GDT (does not work)

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -21,9 +21,25 @@ struct gdt_ptr gp;
 /* From assembly */
 extern void gdt_flush(void);
 
+void gdt_set_gate(size_t index, uint64_t base, uint64_t limit, uint8_t access, uint8_t granularity) {
+	gdt[index].base_low = (base & 0xFFFF);
+	gdt[index].base_middle = (base >> 16) & 0xFF;
+	gdt[index].base_high = (base >> 24) & 0xFF;
+	gdt[index].limit_low = limit & 0xFFFF;
+	gdt[index].granularity = (limit >> 16) & 0x0F;
+	gdt[index].granularity |= granularity & 0xF0;
+	gdt[index].access = access;
+}
+
 void load_gdt() {
 	gp.limit = ((sizeof(struct gdt_entry)) * sizeof(gdt)) - 1;
 	gp.base = (uint32_t)&gdt;
 
-	/* TODO: Actually build and load the GDT */
+	/* NULL gate */
+	gdt_set_gate(0, 0, 0, 0, 0);
+	/* Code segment */
+	gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+	/* Data segment */
+	gdt_set_gate(1, 0, 0xFFFFFFFF, 0x92, 0xCF);
+	gdt_flush();
 }
